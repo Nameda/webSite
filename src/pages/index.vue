9e88d56a862dd7b735e59e658bfc616d70e11eb1
wangting04 <wangting04@cd.tuan800.com>
2019-04-11 14:25:56 +0800

    wt

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <headerBar></headerBar>
+
+  </div>
+</template>
+
+<script>
+import headerBar from './../components/headerBar'
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+      headerBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
