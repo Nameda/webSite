a358a66ded4853031f951d452ffd4243d91357ba
nameda <wang_ting0311@163.com>
2019-05-02 20:14:46 +0800

    wt

--- src/components/botContact.vue
+++ src/components/botContact.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bot-contact">
         <p><i></i>24小时服务专线</p>
-        <p>0871-65342054</p>
+        <a href="tel:0871-65342054">0871-65342054</a>
     </div>
 </template>
 
@@ -13,35 +13,38 @@ export default {
 
 <style lang="scss" scoped>
 .bot-contact{
-     display: none;
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        z-index: 9;
-        width: 100%;
-        height: 12%;
-          background: #267c8b;
-          line-height: 30px;
-          text-align: center;
-          color: #fff;
-          font-size: 24px;
-          cursor: pointer;
-          &:hover{
-            opacity: 0.8;
-          }
-          p:nth-of-type(1){
-            padding-top: 12px;
-            i{
-              display: inline-block;
-              width: 24px;
-              height: 24px;
-              background: url('../assets/imgs/index/phone-icon.png') no-repeat;
-              background-size: 100%;
-              position: relative;
-              top: 4px;
-              right: 8px;
-            }
-          }
+    display: none;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 9;
+    width: 100%;
+    height: 12%;
+    background: #267c8b;
+    line-height: 30px;
+    text-align: center;
+    color: #fff;
+    font-size: 24px;
+    cursor: pointer;
+    &:hover{
+        opacity: 0.8;
+    }
+    p:nth-of-type(1){
+        padding-top: 12px;
+        i{
+            display: inline-block;
+            width: 24px;
+            height: 24px;
+            background: url('../assets/imgs/index/phone-icon.png') no-repeat;
+            background-size: 100%;
+            position: relative;
+            top: 4px;
+            right: 8px;
+        }
+    }
+    a{
+        color:#fff;
+    }
 }
 @media screen and (max-width: 768px){
     .bot-contact {
