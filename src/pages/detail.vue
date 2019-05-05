87e85f5d33c18f63652a9493efac69994442519c
nameda <wang_ting0311@163.com>
2019-05-05 22:03:47 +0800

    wt

--- src/pages/detail.vue
+++ src/pages/detail.vue
@@ -9,7 +9,7 @@
             <p v-for="(item,index) in detailLists[id].intros" :key='index'>{{item}}</p>
           </div>
             <div class="exhibition-box-main row">
-                <div class="moving col-md-4" v-for="(item,index) in lists" :key="index">
+                <div class="moving col-md-4" v-for="(item,index) in detailLists[id].lists" :key="index">
                     <img :src="item.pic" class="exhibition-img">
                     <div class="exhibition-intro">
                         {{item.name}}
@@ -38,53 +38,87 @@ export default {
                 intros:['告别，不是生命的终结，而是生命最美的绽放',
                 '亦生命最宝贵的传承',
                 '一场追忆，一曲华彩，一生记忆'],
-                des:'昆明金铂利殡仪有限公司'
+                des:'昆明金铂利殡仪有限公司',
+                lists:[{
+                    pic:require('../assets/imgs/index/4.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/8.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/6.jpg'),
+                    name:'金铂利详情'
+                }]
             },
             {
                 title:'第2个',
                 intros:['告别，不是生命的终结，而是生命最美的绽放',
                 '亦生命最宝贵的传承',
                 '一场追忆，一曲华彩，一生记忆'],
-                des:'昆明金铂利殡仪有限公司'
+                des:'昆明金铂利殡仪有限公司',
+                lists:[{
+                    pic:require('../assets/imgs/index/4.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/8.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/6.jpg'),
+                    name:'金铂利详情'
+                }]
             },
             {
                 title:'第3个',
                 intros:['告别，不是生命的终结，而是生命最美的绽放',
                 '亦生命最宝贵的传承',
                 '一场追忆，一曲华彩，一生记忆'],
-                des:'昆明金铂利殡仪有限公司'
+                des:'昆明金铂利殡仪有限公司',
+                lists:[{
+                    pic:require('../assets/imgs/index/4.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/8.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/6.jpg'),
+                    name:'金铂利详情'
+                }]
             },
             {
                 title:'第4个',
                 intros:['告别，不是生命的终结，而是生命最美的绽放',
                 '亦生命最宝贵的传承',
                 '一场追忆，一曲华彩，一生记忆'],
-                des:'昆明金铂利殡仪有限公司'
+                des:'昆明金铂利殡仪有限公司',
+                lists:[{
+                    pic:require('../assets/imgs/index/4.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/8.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/6.jpg'),
+                    name:'金铂利详情'
+                }]
             },
             {
                 title:'第5个',
                 intros:['告别，不是生命的终结，而是生命最美的绽放',
                 '亦生命最宝贵的传承',
                 '一场追忆，一曲华彩，一生记忆'],
-                des:'昆明金铂利殡仪有限公司'
+                des:'昆明金铂利殡仪有限公司',
+                lists:[{
+                    pic:require('../assets/imgs/index/4.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/8.jpg'),
+                    name:'金铂利详情'
+                },{
+                    pic:require('../assets/imgs/index/6.jpg'),
+                    name:'金铂利详情'
+                }]
             }
         ],
-        lists:[{
-            pic:require('../assets/imgs/index/4.jpg'),
-            name:'金铂利详情'
-        },{
-            pic:require('../assets/imgs/index/8.jpg'),
-            name:'金铂利详情'
-        },{
-            pic:require('../assets/imgs/index/6.jpg'),
-            name:'金铂利详情'
-        },{
-            pic:require('../assets/imgs/index/7.jpg'),
-            name:'金铂利详情'
-        },{
-            pic:require('../assets/imgs/index/5.jpg'),
-            name:'金铂利详情'
-        }]
     }
   },
   components:{
